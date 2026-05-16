7424571f8b87f631608952871a33d0e5adb245b8
eslam-devops <eslamzain863@gmail.com>
2026-05-16 11:50:50 +0300

    Create  sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+// sw.js
+const CACHE_NAME = 'azkar-v3';
+const urlsToCache = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'icon-192.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
